265225d4cc272580e4b86a736da863cb647a5007
Mücahit Yücel <82612069+Alanzinho61@users.noreply.github.com>
2024-01-29 23:38:25 +0300

    player Tablosu eklendi

---
+++ WebApplication3/Controllers/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    public class PlayerController : Controller
+    {
+        private readonly ClupContext _db;
+        public PlayerController(ClupContext db)
+        {
+            _db = db;          
+        }
+        public IActionResult Index()
+        {
+            return View(_db.Players.ToList());
+        }
+        
+        public IActionResult Add()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Add(Player p)
+        {
+            if (p != null)
+            {
+                _db.Players.Add(p);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Update(int id)
+        {
+            return View(_db.Players.Find(id));
+        }
+        [HttpPost]
+        public IActionResult Update(Player p)
+        {
+            if (p != null)
+            {
+                _db.Players.Update(p);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+        public IActionResult Delete(int id) 
+        {
+            var sil = _db.Players.Find(id);
+            if(sil != null)
+            {
+                _db.Players.Remove(sil);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View();
+        }
+
+    }
+}
